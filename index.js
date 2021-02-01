b5f45df15c68e32a7d2101bce492410c75c064c0
Leonardo Uezu <44908786+luezu-42@users.noreply.github.com>
2021-02-01 08:12:35 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ let sum = [];
 //função para ler o arquivo json
 const itensdata = fs.readFileSync("itens.json");
 
-//Após o fs ler o arquivo ele retorna o buffer(região fisica de memória do hardware) dos dados, para resolver isso usamos o JSON.parse para formatar para json
+//Após o fs ler o arquivo ele retorna o buffer(região fisica de memória do hardware) dos dados, para resolver isso usamos o JSON.parse para formatar em json
 const itens = JSON.parse(itensdata);
 
 const numItens = itens.map((el) =>
-  sum.push(parseFloat(el.quantity * el.value))
+  sum.push(Number(el.quantity * el.value))
 );
 const plus = (x, y) => x + y;
 const sumItens = sum.reduce(plus, 0);
